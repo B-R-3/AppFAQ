3b5cf62d2f8e1f49e5b6fc6e6685ab10380ed6ed
Inedra15 <159150630+Inedra15@users.noreply.github.com>
2024-03-19 13:54:18 +0100

    Add files via upload

--- AppFAQ/Sup.php
+++ AppFAQ/Sup.php
@@ -1,3 +1,38 @@
+<?php
+include "fonction.inc.php";
+session_start();
+
+// Connexion à la base de données
+$dbh = connexion();
+
+$message = "";
+
+// Récupère le contenu du formulaire
+$reponse = isset($_POST['reponse']) ? $_POST['reponse'] : '';
+$question = isset($_POST['question']) ? $_POST['question'] : '';
+$submit = isset($_POST['submit']);
+
+// Vérifie le user
+if ($submit) {
+    $sql = "delete from faq where question=:question and reponse=:reponse";
+    try {
+        $sth = $dbh->prepare($sql);
+        $sth->execute(array(
+            ':question'=>$question,
+            ':reponse'=>$reponse
+    ));
+    } catch (PDOException $ex) {
+        die("Erreur lors de la requête SQL : " . $ex->getMessage());
+    }
+}
+?>
+
+
+
+
+
+
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -23,22 +58,22 @@
     <h1>M2L</h1>
 
     <form id="formulaire" action="contact.php">
-        <label for="message">Question</label> <br>
-        <textarea name="message" id="message" cols="50" rows="10"> Est ce que le ballon est rond ?</textarea>
+        <label for="question">Question</label> <br>
+        <textarea name="question" id="question" cols="50" rows="10"> Est ce que le ballon est rond ?</textarea>
 
 
     </form>
 
 
     <form id="formulaire" action="contact.php">
-        <label for="message">Réponse</label> <br>
-        <textarea name="message" id="message" cols="50" rows="10"> Bien sur que oui</textarea>
+        <label for="reponse">Réponse</label> <br>
+        <textarea name="reponse" id="reponse" cols="50" rows="10"> Bien sur que oui</textarea>
 
 
     </form>
     <div class="button">
-        <a href="list.php"><input type="button" id="v" value="Supprimer"></a>
-        <a href="list.php"><input type="button" id="f" value="Annuler"> <br></a>
+        <p><input type="submit" id="v" name="submit" value="Supprimer" />
+        <a href="list.php"><input type="button" id="f" value="Annuler"> <br></a></p>
 
 
 
